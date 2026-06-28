1f1aad0ed78420f23be62614381d0aebc7b6ff80
Niccolo Sacchi <niccsacchi@gmail.com>
2026-06-28 15:37:03 +0200

    Make get_shared_assets_path work from wherever.

--- cs336_data/common.py
+++ cs336_data/common.py
@@ -7,7 +7,9 @@ MODAL_SHARED_PATH = Path("/shared-data")
 
 def get_shared_assets_path() -> Path:
     if modal.is_local():
-        (local_path := Path("local-shared-data").resolve()).mkdir(exist_ok=True, parents=True)
+        workspace_root = Path(__file__).resolve().parent.parent
+        local_path = workspace_root / "local-shared-data"
+        local_path.mkdir(exist_ok=True, parents=True)
         return local_path
     else:
         return MODAL_SHARED_PATH
