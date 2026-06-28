0f064742f57846abcf0b0616774904a78d0a1ca0
Niccolo Sacchi <niccsacchi@gmail.com>
2026-06-28 15:49:56 +0200

    Implement language classification.

--- cs336_data/data_processing.py
+++ cs336_data/data_processing.py
@@ -1,6 +1,45 @@
 from fastwarc.warc import ArchiveIterator, WarcRecordType
 from resiliparse.extract.html2text import extract_plain_text
 from resiliparse.parse.encoding import detect_encoding
+import fasttext
+from pathlib import Path
+from cs336_data.common import get_shared_assets_path
+
+_FASTTEXT_MODEL = None
+
+
+def get_fasttext_model():
+    global _FASTTEXT_MODEL
+
+    if _FASTTEXT_MODEL is not None:
+        return _FASTTEXT_MODEL
+
+    model_path = get_shared_assets_path() / "classifiers" / "lid.176.bin"
+    if not model_path.exists():
+        raise FileNotFoundError(
+            f"fastText language identification model not found at {model_path}"
+        )
+    _FASTTEXT_MODEL = fasttext.load_model(str(model_path))
+    return _FASTTEXT_MODEL
+
+
+def identify_language(text: str) -> tuple[str, float]:
+    cleaned_text = text.replace("\n", " ")
+    model = get_fasttext_model()
+    labels, probabilities = model.predict(cleaned_text, k=1)
+    if not labels:
+        raise ValueError(f"model.predict({cleaned_text}) returned no prediction")
+    if len(labels) != 1:
+        raise ValueError(
+            f"model.predict({cleaned_text}) unexpectedly did not return exactly one prediction: {labels}, {probabilities}"
+        )
+    label = labels[0]
+    score = float(probabilities[0])
+    if label.startswith("__label__"):
+        lang = label[len("__label__") :]
+    else:
+        lang = label
+    return lang, score
 
 
 def extract_text_from_warc(warc_file_path, n=10):
